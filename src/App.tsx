fc3b08e06ec4a75feb2e9901256415e9ceb066f7
Slawomir Niemiec <slawekniemiec@gmail.com>
2023-03-25 21:25:14 +0100

    clean boilerplate and setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
+
+type DirectionType = "N" | "E" | "S" | "W";
+interface Robot {
+  x: number;
+  y: number;
+  robotNumber: number;
+  direction: DirectionType;
+}
+
+const initialRobot: Robot = { x: 0, y: 0, direction: "N", robotNumber: 1 };
 
 function App() {
+  const [robot, setRobot] = useState<Robot>(initialRobot);
+  const instructionsRef = useRef<HTMLInputElement>(null);
+
+  const executeInstructions = (instructions: string) => {
+    const { x, y, direction, robotNumber } = robot;
+
+    let updatedX = x;
+    let updatedY = y;
+    let updatedRobotNumber = robotNumber;
+    let updatedDirection = direction;
+
+    for (const instruction of instructions) {
+      if (instruction === "L" || instruction === "R") {
+
+        const directionMap: Record<
+          DirectionType,
+          Record<"L" | "R", DirectionType>
+        > = {
+          N: { L: "W", R: "E" },
+          E: { L: "N", R: "S" },
+          S: { L: "E", R: "W" },
+          W: { L: "S", R: "N" },
+        };
+
+        updatedDirection = directionMap[updatedDirection][instruction];
+      } else if (instruction === "F") {
+        switch (updatedDirection) {
+          case "N":
+            updatedY++;
+            break;
+          case "E":
+            updatedX++;
+            break;
+          case "S":
+            updatedY--;
+            break;
+          case "W":
+            updatedX--;
+            break;
+        }
+      }
+    }
+
+    setRobot({
+      x: updatedX,
+      y: updatedY,
+      robotNumber: updatedRobotNumber,
+      direction: updatedDirection,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const instructionsValue = instructionsRef.current?.value;
+
+    if (!instructionsValue) return;
+
+    const upcasedTrimmedInstructions = instructionsValue.toUpperCase().replace(/\s/g, "");
+
+    executeInstructions(upcasedTrimmedInstructions);
+
+    instructionsRef.current?.value && (instructionsRef.current.value = "");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>ROBOTS ON MARS</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={instructionsRef}
+          placeholder="Enter input: L, R, F"
+        />
+        <button type="submit">SUBMIT INSTRUCTIONS</button>
+      </form>
+      <p>ROBOT{robot.robotNumber} output:</p>
+      <p>
+        {robot.x} {robot.y} {robot.direction}
+      </p>
     </div>
   );
 }
